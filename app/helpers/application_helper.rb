c2cc80fc4568de78f9ef0d6b75ce1562b75fb2b5
Jacky Alciné <me@jalcine.me>
2013-11-16 18:25:00 -0500

    Added some awesome jazz.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def page_title(title = nil)
+    if title.nil?
+      return content_for(:page_title)
+    else
+      content_for(:page_title, "#{title} - Vettio")
+    end
+  end
+
+  def navigation_state(state = nil)
+    if state.nil?
+      return content_for(:navigation_state)
+    else
+      content_for(:navigation_state, state)
+    end
+  end
 end
