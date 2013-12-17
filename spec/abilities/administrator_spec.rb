fbabe6a42c37ccb6b3412825d7c866a4eeb0277c
Jacky Alciné <jalcine@vettio.com>
2013-12-17 12:34:07 -0500

    Updated jazz.

---
+++ spec/abilities/administrator_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe :administrator do
+  subject { FactoryGirl.create :administrator_user }
+  let(:ability) { Ability.new(subject) }
+
+  describe Event do
+    let(:event) { FactoryGirl.create :event }
+    it { expect(ability.can?(:manage, event)).to eq(true) }
+  end
+
+  describe Ticket do
+    let(:ticket) { FactoryGirl.create :ticket }
+    it { expect(ability.can?(:manage, ticket)).to eq(true) }
+  end
+
+  describe Order do
+    let(:order) { FactoryGirl.create :order }
+    it { expect(ability.can?(:manage, order)).to eq(true) }
+  end
+end
---
+++ spec/abilities/attendee_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe :attendee do
+  subject { FactoryGirl.create :attendee_user }
+  let(:ability) { Ability.new(subject) }
+  
+  describe Event do
+    let(:event) { FactoryGirl.create :event }
+    it { expect(ability.can?(:view, event)).to eq(true) }
+    it { expect(ability.can?(:rsvp, event)).to eq(true) }
+    it { expect(ability.can?(:crud, event)).to eq(false) }
+  end
+
+  describe Ticket do
+    let(:ticket) { FactoryGirl.create :ticket }
+    it { expect(ability.can?(:view, ticket)).to eq(true) }
+    it { expect(ability.can?(:order, ticket)).to eq(true) }
+    it { expect(ability.can?(:crud, ticket)).to eq(false) }
+  end
+
+  describe Order do
+    let(:order) { FactoryGirl.create :order }
+    it { expect(ability.can?(:create, order).to eq(true)) }
+    it { expect(ability.can?(:cancel, order).to eq(true)) }
+  end
+end
