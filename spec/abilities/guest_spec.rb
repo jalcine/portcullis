845203af5a5b4bd916cbd1815f89aee9b632a6f1
Jacky Alciné <jalcine@vettio.com>
2013-12-17 02:08:18 -0500

    Updated guest spec with testing of order controls.

--- spec/abilities/guest_spec.rb
+++ spec/abilities/guest_spec.rb
@@ -15,4 +15,10 @@ describe :guest do
     it { expect(ability.can?(:view, ticket)).to eq(true) }
     it { expect(ability.can?(:crud, ticket)).to eq(false) }
   end
+
+  describe Order do
+    let(:order) { FactoryGirl.create :order }
+    it { except(ability.can?(:create, order)).to eq(false) }
+    it { except(ability.can?(:cancel, order)).to eq(false) }
+  end
 end
