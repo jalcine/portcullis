45613168bd0a84a5dbb39167af163d34848d8c23
Jacky Alciné <jalcine@vettio.com>
2013-12-17 01:28:20 -0500

    Refined tests of abilities.

--- spec/abilities/guest_spec.rb
+++ spec/abilities/guest_spec.rb
@@ -7,14 +7,12 @@ describe :guest do
   describe Event do
     let(:event) { FactoryGirl.create :event }
     it { expect(ability.can?(:view, event)).to eq(true) }
-    it { expect(ability.can?(:modify, event)).to eq(false) }
     it { expect(ability.can?(:crud, event)).to eq(false) }
   end
 
   describe Ticket do
     let(:ticket) { FactoryGirl.create :ticket }
     it { expect(ability.can?(:view, ticket)).to eq(true) }
-    it { expect(ability.can?(:modify, ticket)).to eq(false) }
     it { expect(ability.can?(:crud, ticket)).to eq(false) }
   end
 end
