303d090ba2586687e0e4eb9ca2076a61118c6f91
Jacky Alciné <jalcine@vettio.com>
2013-12-17 01:20:22 -0500

    Defined specs for guest role.

--- spec/abilities/guest_spec.rb
+++ spec/abilities/guest_spec.rb
@@ -1,17 +1,20 @@
 require 'spec_helper'
 
-describe 'Guest' do
+describe :guest do
   subject { FactoryGirl.create :guest_user }
+  let(:ability) { Ability.new(subject) }
 
   describe Event do
     let(:event) { FactoryGirl.create :event }
-    it { expect { subject.can? :view,   event }.to be_true }
-    it { expect { subject.can? :modify, event }.to be_false }
+    it { expect(ability.can?(:view, event)).to eq(true) }
+    it { expect(ability.can?(:modify, event)).to eq(false) }
+    it { expect(ability.can?(:crud, event)).to eq(false) }
   end
 
   describe Ticket do
     let(:ticket) { FactoryGirl.create :ticket }
-    it { expect { subject.can? :view,   ticket }.to be_true }
-    it { expect { subject.can? :modify, ticket }.to be_false }
+    it { expect(ability.can?(:view, ticket)).to eq(true) }
+    it { expect(ability.can?(:modify, ticket)).to eq(false) }
+    it { expect(ability.can?(:crud, ticket)).to eq(false) }
   end
 end
