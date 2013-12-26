8f75214cf0ca05fd9f2491c717608efbb76a107e
Jacky Alciné <jalcine@vettio.com>
2013-12-26 05:02:47 -0500

    Updated misc steps for acceptance.

--- spec/acceptance/steps/miscelleanous_steps.rb
+++ spec/acceptance/steps/miscelleanous_steps.rb
@@ -1,11 +1,21 @@
 module MiscelleanousSteps
   step 'I should see an error' do
+    flash_gordon = find '#flash_gordon'
+    expect(flash_gordon.first('.warning')).to be_nil
   end
 
   step 'I should see a message indicating :message' do | message |
+    within '#flash_gordon' do
+      expect(page).to_not have_content(message)
+    end
   end
 
-  step 'I should not see the event called :name in the event list' do
+  step 'I should not see the text :content on the page' do | content |
+    expect(page).to_not have_content(content)
+  end
+
+  step 'I should see the text :content on the page' do | content |
+    expect(page).to have_content(content)
   end
 end
 
