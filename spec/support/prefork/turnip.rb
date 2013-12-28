9dc9f1cecf5d98f1fc5893a8e71546d04b6f712d
Jacky Alciné <jalcine@vettio.com>
2013-12-28 04:23:02 -0500

    Moved turnip jazz to be part of preforking.

--- spec/support/prefork/turnip.rb
+++ spec/support/prefork/turnip.rb
@@ -1,1 +1,8 @@
-require 'turnip/kanban'
+require 'turnip/rspec'
+require 'turnip/capybara'
+
+RSpec.configure do | config |
+  Dir.glob("#{Rails.root}/spec/acceptance/steps/*.rb") do | file |
+    require file
+  end
+end
