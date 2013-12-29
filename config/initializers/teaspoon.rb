64d41a118ea8283cca22b69939e5ceac7418a1c4
Jacky Alciné <jalcine@vettio.com>
2013-12-29 00:54:40 -0500

    Added Teaspoon configuration for Rails.

---
+++ config/initializers/teaspoon.rb
@@ -0,0 +1,20 @@
+Teaspoon.setup do |config|
+  config.mount_at = "/teaspoon"
+  config.root = nil
+  config.asset_paths = ["spec/javascripts", "spec/javascripts/stylesheets"]
+  config.fixture_path = "spec/javascripts/fixtures"
+
+  config.suite do |suite|
+    suite.matcher = "{spec/javascripts,app/assets}/**/*_spec.{js,js.coffee,coffee}"
+    suite.helper = "spec_helper"
+    suite.javascripts = ['teaspoon/mocha']
+    suite.stylesheets = ["teaspoon"]
+    suite.no_coverage = [%r{/lib/ruby/gems/}, %r{/vendor/assets/}, %r{/support/}, %r{/(.+)_helper.}]
+    suite.no_coverage << "jquery.js" # excludes jquery from coverage reports
+
+  end
+
+  config.suite :quirks do |suite|
+    suite.matcher = "spec/javascripts/quirks/*_spec.{js,js.coffee,coffee}"
+  end
+end if defined?(Teaspoon) && Teaspoon.respond_to?(:setup)
--- spec/teaspoon_env.rb
+++ spec/teaspoon_env.rb
@@ -2,23 +2,22 @@ ENV["RAILS_ROOT"] = File.expand_path("../../", __FILE__)
 require File.expand_path("../../config/environment", __FILE__)
 
 Teaspoon.setup do |config|
-  config.server :poltergeist
-
   # Behaviors
-  config.server_timeout      = 20 # timeout for starting the server
-  config.server_port         = nil # defaults to any open port unless specified
-  config.fail_fast           = true # abort after the first failing suite
+  config.server_timeout = 20 # timeout for starting the server
+  config.server_port = 4374 # defaults to any open port unless specified
+  config.fail_fast = true # abort after the first failing suite
 
   # Output
-  config.formatters          = "tap"
-  config.suppress_log        = true
-  config.color               = true
+  config.formatters = "tap"
+  config.suppress_log = true
+  config.color = true
 
-  config.coverage                      = true
-  config.coverage_reports              = "text,html,cobertura"
-  config.coverage_output_dir           = "coverage"
+  # Coverage
+  config.coverage = true
+  config.coverage_reports = "text,html,cobertura"
+  config.coverage_output_dir = "coverage"
   config.statements_coverage_threshold = 50
-  config.functions_coverage_threshold  = 50
-  config.branches_coverage_threshold   = 50
-  config.lines_coverage_threshold      = 50
+  config.functions_coverage_threshold = 50
+  config.branches_coverage_threshold = 50
+  config.lines_coverage_threshold = 50
 end
