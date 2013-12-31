39a0d8e7c143d2169146fd74d4969da27767a236
Jacky Alciné <jalcine@vettio.com>
2013-12-30 19:41:46 -0500

    Tweaks and experiments for Capybara tests.

--- spec/acceptance/steps/tickets_steps.rb
+++ spec/acceptance/steps/tickets_steps.rb
@@ -1,25 +1,44 @@
 module TicketSteps
   step 'I add :number :type tickets to the event named :ticket' do | number, type, ticket |
     click_link 'Add Tickets'
-    expect(page).to have_content 'Ticket For Event'
+    expect(page).to have_content 'Add Ticket For Event'
 
-    case type.downcase.to_s
-    when :free
-      click_link 'Free', exact: true
-    when :paid
-      click_link 'Paid', exact: true
-    when :donation
-      click_link 'Donation', exact: true
+    within 'form[data-ticket]' do
+      case type.downcase.to_s
+      when :free
+        click_link 'Free', exact: true
+      when :paid
+        click_link 'Paid', exact: true
+      when :donation
+        click_link 'Donation', exact: true
+      end
+
+      fill_in 'ticket[name]',        with: ticket
+      fill_in 'ticket[quantity]',    with: number
+      fill_in 'ticket[description]', with: Faker::Lorem.paragraphs(3).join("\n")
+
+      first('#ticket_day_start').click
+      screenshot_and_open_image
+      click_link '10'
+      first('#ticket_time_start').click
+      click_link '8:00 AM'
+      first('#ticket_day_end').click
+      click_link '10'
+      first('#ticket_time_end').click
+      click_link '5:00 PM'
+
+      find_button('Save Ticket').trigger 'click'
+      expect(page).to have_content 'Saving Ticket'
+      expect(page).to_not have_content 'Save Ticket'
     end
-
-    fill_in 'ticket[name]',        with: ticket
-    fill_in 'ticket[quantity]',    with: number
-    fill_in 'ticket[description]', with: Faker::Lorem.paragraphs(3).join("\n")
-    find('form[data-ticket] button[type=submit]').trigger 'click'
+    expect('form[data-ticket]').to_not be_visible
+    screenshot_and_open_image
   end
 
   step 'the event has a :type ticket named :name' do | type, name |
-    expect(page).to have_content(name)
+    within 'li[data-ticket]' do
+      expect(page).to have_content(name)
+    end
   end
 
   step 'I click to edit the ticket named :name' do | name |
