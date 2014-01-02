e258e399d58c4f9de62c5a73faa7ef9f3700656e
Jacky Alciné <jalcine@vettio.com>
2014-01-02 04:36:01 -0500

    Made this a bit more lighter.

--- spec/views/components/_flash_gordon.html.haml_spec.rb
+++ spec/views/components/_flash_gordon.html.haml_spec.rb
@@ -3,16 +3,15 @@ require 'spec_helper'
 describe 'components/_flash_gordon.html.haml' do
   describe 'displaying flash messages' do
     [:notice, :alert, :error].each do | flash_type |
+      let(:message) { "This is a #{flash_type.to_s} message." }
       before(:each) do
-        flash[flash_type] = "This is a #{flash_type.to_s}."
+        flash[flash_type] = message
         allow(view).to receive(:resource_name).and_return(:user)
-        render template: 'layouts/application', locals: { }
+        render 'components/flash_gordon', locals: { }
       end
 
-      it { expect(rendered).to have_selector 'body header.main[role=contentinfo] > .flash_gordon' }
-      it { expect(rendered).to have_selector "body header.main[role=contentinfo] > .flash_gordon > .#{flash_type}" }
-      it { expect(rendered).to match /This is a / }
-      it { expect(rendered).to match flash_type.to_s }
+      it { expect(rendered).to have_selector ".flash_gordon > .#{flash_type}" }
+      it { expect(rendered).to have_content message }
     end
   end
 end
