9865fbb317e18c5d6a154222b806e6fecc1a3229
Jacky Alciné <jalcine@vettio.com>
2014-01-02 22:08:50 -0500

    Updated steps for authentication.

--- spec/acceptance/steps/authentication_steps.rb
+++ spec/acceptance/steps/authentication_steps.rb
@@ -1,16 +1,4 @@
 module AuthenticationSteps
-  step 'I sign in with :provider' do | provider |
-    send 'I go to the sign-in page'
-    expect(find("a.#{provider.downcase}")).to_not be_nil
-    find("a.#{provider.downcase}").trigger 'click'
-  end
-
-  step 'I sign up with :provider' do | provider |
-    send 'I go to the sign-up page'
-    expect(find("a.#{provider.downcase}")).to_not be_nil
-    find("a.#{provider.downcase}").trigger 'click'
-  end
-
   step 'a :role is signed in' do | role = 'guest' |
     @user = create :user, role.to_sym
     send 'I go to the sign-in page'
@@ -21,6 +9,23 @@ module AuthenticationSteps
     @user = build :user, role.to_sym
     send 'I go to the sign-up page'
     send 'I sign up with a new account'
+    @user.add_role role
+  end
+
+  step 'I sign in with an existing account' do
+    send 'I go to the sign-in page'
+    send 'I enter my e-mail address and password'
+    send 'I click the "Sign In" button'
+    send 'I am signed in'
+  end
+
+  step 'I sign up with a new account' do
+    send 'I go to the sign-up page'
+    send 'I enter my e-mail address and password'
+    send 'I enter my name'
+    send 'I enter my password confirmation'
+    step 'I click the "Sign Up" button'
+    send 'I am signed up'
   end
 
   step 'the user signs out' do
@@ -30,14 +35,6 @@ module AuthenticationSteps
 
   step 'I have a pre-existing user account' do
     @user = create :user
-  end
-
-  step 'the provider is bound to fail' do
-    pending 'make provider creation fail'
-  end
-
-  step 'A new user should be created from data from :provider' do | provider |
-    pending "Handle logic for determing existence from #{provider} data."
   end
 
   step 'I enter my e-mail address and password' do
@@ -51,30 +48,21 @@ module AuthenticationSteps
   end
 
   step 'I enter my password confirmation' do
-    fill_in 'Password confirmation', with: @user.password
-  end
-
-  step 'I sign up for a new account' do
-    send 'I enter my e-mail address and password'
-    send 'I enter my password confirmation'
-    send 'I click the "Sign Up" button'
-    send 'I am signed up'
-  end
-
-  step 'I sign in with an existing account' do
-    send 'I enter my e-mail address and password'
-    send 'I click the "Sign In" button'
-    send 'I am signed in'
+    fill_in 'user[password_confirmation]', with: @user.password
   end
 
   step 'I am signed in' do
     expect(page).to have_content 'Sign Out'
     expect(page).to have_content 'Signed in successfully.'
+    @user = User.find_by_email(@user.email)
+    expect(@user).to_not be_nil
   end
 
   step 'I am signed up' do
     expect(page).to have_content 'Sign Out'
     expect(page).to have_content 'signed up successfully.'
+    @user = User.find_by_email(@user.email)
+    expect(@user).to_not be_nil
   end
 end
 
