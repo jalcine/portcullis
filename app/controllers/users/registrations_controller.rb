cbb56c969023db30a6f6817d55da39d09ce58f5d
Jacky Alciné <jalcine@vettio.com>
2014-01-02 22:09:45 -0500

    Ensured users have a profile on creation.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,2 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  def create
+    super
+    current_user.create_profile! first_name: params[:profile][:first_name],
+      last_name: params[:profile][:last_name]
+  end
 end
