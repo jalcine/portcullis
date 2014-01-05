c7a029817fc915669695a075f4536bd1d541f6c0
Jacky Alciné <jalcine@vettio.com>
2014-01-05 02:22:38 -0500

    Touched up configuration.

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ Bundler.require(:default, Rails.env)
 module Portcullis
   class Application < Rails::Application
     config.time_zone = 'Eastern Time (US & Canada)'
-    config.i18n.default_locale = :en
     config.middleware.use Rack::Runtime
     config.middleware.use ActionDispatch::RemoteIp
   end
