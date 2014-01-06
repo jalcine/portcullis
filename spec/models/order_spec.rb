3027a957b12c485cd2f1a8731f56d7d38b69e5c8
Jacky Alciné <jalcine@vettio.com>
2014-01-06 10:01:12 -0500

    Updated Order model-spec.

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -5,14 +5,47 @@ describe Order do
     subject { FactoryGirl.create :order }
     it { expect(subject).to have(:no).errors_on(:ticket) }
     it { expect(subject).to have(:no).errors_on(:user) }
+    it { expect(subject).to have(:no).errors_on(:transaction) }
+    it { expect(subject).to have(:no).errors_on(:charge) }
   end
 
-  describe 'processing' do
-    let(:ticket) { FactoryGirl.create :ticket }
-    let(:user) { FactoryGirl.create :user }
-    it 'starts processing a new order' do
-      order = Order.create! ticket: ticket, user: user
-      expect(order).to be_processing
+  describe '.new' do
+    describe 'creates a new order for a priced ticket' do
+      let(:ticket) { create(:ticket, :free) }
+      before(:each) do
+        @order = Order.new user: create(:user, :attendee),
+          ticket: ticket, quantity: 6
+        @order.save
+      end
+
+      it { expect(@order).to have(:no).errors }
+
+      it 'doesnt have a transaction ' do
+        expect(@order.transaction).to be_nil
+      end
+
+      it 'must have a zero charge' do
+        expect(@order.charge).to be_zero
+      end
+    end
+
+    describe 'creates a new order for a priced ticket' do
+      let(:ticket) { create(:ticket, :priced) }
+      before(:each) do
+        @order = Order.new user: create(:user, :attendee),
+          ticket: ticket, quantity: 6
+        @order.save
+      end
+
+      it { expect(@order).to have(:no).errors }
+
+      it 'doesnt have a transaction ' do
+        expect(@order.transaction).to be_nil
+      end
+
+      it 'has a nonzero charge' do
+        expect(@order.charge).to_not be_zero
+      end
     end
   end
 end
