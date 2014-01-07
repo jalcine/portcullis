a13d2bf626f3ed30ccb354cdebea41e9cc44b6b8
Jacky Alciné <jalcine@vettio.com>
2014-01-07 04:38:47 -0500

    Updated jazz all around.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,6 +28,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     request.referrer if request.referrer.present?
+    Rails.logger.debug "OAUTH GO BACK: #{request.env['omniauth.origin']}"
     request.env['omniauth.origin'] if request.env.include? 'omniauth.origin'
     events_url(scope: :recommended)
   end
--- app/uploaders/event_banner_uploader.rb
+++ app/uploaders/event_banner_uploader.rb
@@ -1,12 +1,13 @@
 class EventBannerUploader < ImageUploader
    version :boxed do
-     process resize_to_fill: [256, 256]
+     process resize_to_fill: [256, 192]
    end
 
    version :masthead do
+     process resize_to_fill: [1024, 768]
    end
 
   def default_url
-    ActionController::Base.helpers.asset_path('placeabo.jpg')
+    ActionController::Base.helpers.asset_path('placebo.jpg')
   end
 end
