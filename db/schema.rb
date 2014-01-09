fb9ff781ba2c2bb1ebab2db7dc6fac742cb10a0d
Jacky Alciné <jalcine@vettio.com>
2014-01-09 02:02:46 -0500

    Updated schema.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140106144013) do
+ActiveRecord::Schema.define(version: 20140109065703) do
 
   create_table "age_groups", force: true do |t|
     t.string   "name"
@@ -65,12 +65,14 @@ ActiveRecord::Schema.define(version: 20140106144013) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "status",     default: 0
-    t.integer  "quantity",   default: 0
-    t.decimal  "charge",     default: 0.0
+    t.integer  "status",         default: 0
+    t.integer  "quantity",       default: 0
+    t.decimal  "charge",         default: 0.0
+    t.integer  "transaction_id"
   end
 
   add_index "orders", ["ticket_id"], name: "index_orders_on_ticket_id", using: :btree
+  add_index "orders", ["transaction_id"], name: "index_orders_on_transaction_id", using: :btree
   add_index "orders", ["user_id"], name: "index_orders_on_user_id", using: :btree
 
   create_table "profiles", force: true do |t|
@@ -123,6 +125,11 @@ ActiveRecord::Schema.define(version: 20140106144013) do
   end
 
   add_index "tickets", ["event_id"], name: "index_tickets_on_event_id", using: :btree
+
+  create_table "transactions", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.datetime "created_at"
