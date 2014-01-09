eee2713313fb88498a60fdbe66dd0da6c4d63ef2
Jacky Alciné <jalcine@vettio.com>
2014-01-09 12:50:42 -0500

    Updated transaction model.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,12 +1,27 @@
 class Transaction < ActiveRecord::Base
   has_many :orders
   belongs_to :merchant, class_name: User, foreign_key: :merchant_id
-  after_save :readonly!
+  after_save :readonly!, if: -> { braintree_transaction_id.present? }
 
   def authorize!
+    return if authorized?
+    result = Braintree::Transaction.sale(
+      amount: 100.00,
+      credit_card: {
+        number: 4444444444444448,
+        expiration_date: "05/14"
+      }
+    ) 
+
+    debugger
+
+    braintree_transaction_id = result.transaction.id if result.success?
+    save!
+    result.success?
   end
 
   def settle!
+    raise NotImplementedError
   end
 
   def service_fee
@@ -14,10 +29,12 @@ class Transaction < ActiveRecord::Base
   end
 
   def authorized?
+    puts ap(braintree_transaction)
     braintree_transaction.status == :authorized
   end
 
   def settled?
+    return false unless readonly?
     braintree_transaction.status == :settled
   end
 
@@ -27,6 +44,9 @@ class Transaction < ActiveRecord::Base
 
   private
   def braintree_transaction
-    Braintree::Transaction.find(self.braintree_transaction_id)
+    return nil if braintree_transaction_id.nil?
+    result = Braintree::Transaction.find(braintree_transaction_id)
+    puts ap(result)
+    result
   end
 end
