53aab5dc716702e0174f2d14607b1f257e65df28
Jacky Alciné <jalcine@vettio.com>
2014-01-09 03:08:05 -0500

    Defined invocations for Transaction, hooking to Braintree.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,32 @@
 class Transaction < ActiveRecord::Base
   has_many :orders
   belongs_to :merchant, class_name: User, foreign_key: :merchant_id
+  after_save :readonly!
+
+  def authorize!
+  end
+
+  def settle!
+  end
+
+  def service_fee
+    braintree_transaction.service_fee
+  end
+
+  def authorized?
+    braintree_transaction.status == :authorized
+  end
+
+  def settled?
+    braintree_transaction.status == :settled
+  end
+
+  def amount
+    braintree_transaction.amount
+  end
+
+  private
+  def braintree_transaction
+    Braintree::Transaction.find(self.braintree_transaction_id)
+  end
 end
