966e5b310a4ef12a8ac45b61b7f549277d385539
Jacky Alciné <jalcine@vettio.com>
2014-01-09 18:35:26 -0500

    Updated controllers and routing.

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -5,29 +5,35 @@ describe OrdersController do
     before(:each) { login_user(create(:user, :attendee))}
     let(:ticket) { create(:ticket) }
 
-    describe 'GET new', broken: true do
-      before(:each) { get :new }
-      it { expect(assigns(:ticket)).to eq(ticket) }
-      it { expect(response.code).to eql(200) }
-      it { expect(response).to render_template 'orders/_form' }
-      it { expect(response).to render_template 'tickets/_slip' }
-    end
-
     describe 'POST create' do
-      before(:each) { post :create, ticket: ticket.id, order: { quantity: ticket.max_quantity } }
+      let(:ticket) { create(:ticket, :priced, event: create(:event)) }
+      before(:each) { post :create, { ticket_id: ticket.id, order: { quantity: ticket.max_quantity } } }
 
+      it { expect(response).to_not redirect_to(root_path) }
       it { expect(assigns(:ticket)).to eq(ticket) }
+      it { expect(assigns(:order)).to be_a_kind_of(Order) }
       it { expect(assigns(:order)).to have(:no).errors }
       it { expect(response.status).to eql(302) }
       it { expect(response).to redirect_to(order_path(assigns(:order))) }
     end
 
     describe 'GET show' do
-      let(:order) { create(:order, ticket: ticket) }
-      before(:each) { get :show, id: order }
-      it { expect(assigns(:order)).to eq(order) }
-      it { expect(response.status).to eql(302) }
-      xit { expect(response).to render_template 'tickets/slip' }
+      describe 'user own order' do
+        let(:order) { create(:order, ticket: ticket, user: controller.current_user) }
+        before(:each) { get :show, id: order }
+        it { expect(assigns(:order)).to eq(order) }
+        it { expect(response.status).to eql(200) }
+        it { expect(response).to render_template 'tickets/_slip' }
+        it { expect(response).to render_template 'orders/show' }
+      end
+
+      describe 'user doesnt own order' do
+        let(:order) { create(:order, ticket: ticket, user: create(:user, :attendee)) }
+        before(:each) { get :show, id: order }
+        it { expect(assigns(:order)).to eq(order) }
+        it { expect(response.status).to eql(302) }
+        it { expect(response).to redirect_to(root_path) }
+      end
     end
 
     describe 'GET edit', broken: true do
@@ -37,6 +43,7 @@ describe OrdersController do
       it { expect(assigns(:order)).to eq(order) }
       it { expect(response).to render_template 'tickets/_slip' }
       it { expect(response).to render_template 'orders/_form' }
+      it { expect(response).to render_template 'orders/edit' }
     end
 
     describe 'PATCH/PUT update', broken: true do
