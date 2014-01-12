e37a5cd955861cfbad592e529308596a8beb373d
Jacky Alciné <jalcine@vettio.com>
2014-01-12 08:11:53 -0500

    Updated controllers.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,14 +7,12 @@ class ApplicationController < ActionController::Base
   extend ApplicationHelper
 
   # Ensure CanCan logic is employed.
-  rescue_from CanCan::AccessDenied do |exception|
-    message = t('authorization.failure')
-    Rails.logger.debug ap(exception)
-    Rails.logger.warn "Authorization failure!"
+  rescue_from CanCan::AccessDenied do |e|
+    Rails.logger.warn "Authorization failure! Attempted to #{e.action} a #{e.subject}"
     respond_to do | format |
-      format.html { redirect_to root_url, alert: message }
-      format.json { render json: message, status: :forbidden }
-      format.js   { render json: message, status: :forbidden }
+      format.html { redirect_to root_url, alert: e.message }
+      format.json { render json: e.message, status: :forbidden }
+      format.js   { render json: e.message, status: :forbidden }
     end
   end
 
@@ -30,7 +28,6 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     request.referrer if request.referrer.present?
-    Rails.logger.debug "OAUTH GO BACK: #{request.env['omniauth.origin']}"
     request.env['omniauth.origin'] if request.env.include? 'omniauth.origin'
     events_url(scope: :recommended)
   end
