ac6121114e16b1e992f99b707da17c4a5ef61e26
Jacky Alciné <jalcine@vettio.com>
2014-01-14 10:38:35 -0500

    Added a monkey.

---
+++ lib/net/http/response.rb
@@ -0,0 +1,20 @@
+module DecodeHttpResponseOverride
+  def initialize(h,c,m)
+    super(h,c,m)
+    @decode_content = true
+  end
+
+  def body
+    res = super
+    if self['content-length']
+      self['content-length']= res.bytesize
+    end
+    res
+  end
+end
+
+module Net
+  class HTTPResponse
+    prepend DecodeHttpResponseOverride
+  end
+end
