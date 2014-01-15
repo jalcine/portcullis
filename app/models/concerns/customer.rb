8f1bac20bc4b7b808ed864dfdfecac1e6c8b9bac
Jacky Alciné <jalcine@vettio.com>
2014-01-15 10:21:31 -0500

    Added a means of obtaining Braintree::Customer.

--- app/models/concerns/customer.rb
+++ app/models/concerns/customer.rb
@@ -5,6 +5,15 @@ module Customer
     after_create :connect_with_braintree
   end
 
+  public
+  def to_customer
+    connect_with_braintree if braintree_customer_id.nil?
+    return Braintree::Customer.search do | search |
+      search.id.is braintree_customer_id
+      search.email.is email
+    end
+  end
+
   private
   def connect_with_braintree
     result = Braintree::Customer.create
